ff9db553525db44994f1375023ea66046db36ba9
Rohitjangid17 <jangidrohit817@gmail.com>
2024-08-23 22:16:14 +0530

    after save button click it will redirect on homepage

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,11 +15,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  createStoreForm!: FormGroup;
 
+  constructor(
+    private _router: Router
+  ) { }
+
+  // create store
+  createStore = () => {
+    this._router.navigate(["/dashboard"]);
+  }
 }
